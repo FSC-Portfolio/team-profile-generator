74dfc07b2d153b2e9ecf2128be28d56e46e486ea
jayarghargh <jayarghargh@gmail.com>
2021-02-01 10:23:39 +1030

    completed Employee class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,8 +1,33 @@
 const Employee = require('../lib/Employee');
 
 describe("Employee Class", () => {
-	it('should create an employee', () => {
-		const employee = new Employee();
-		expect(employee).toBe(Employee);
+	const employee = new Employee("Justin", 1001, 'test@gmail.com');
+	it('should have a name', () => {
+		const employeeName = "Justin"
+		expect(employee.name).toBe(employeeName);
+	});
+	it('should have an id', () => {
+		const id = 1001;
+		expect(employee.id).toEqual(id);
+		expect(typeof employee.id).toBe('number');
+	});
+	it('should have an email', () => {
+		const email = 'test@gmail.com';
+		expect(employee.email).toBe(email);
+	});
+	it('should return its name', () => {
+		const name = "Justin";
+		expect(employee.getName()).toBe(name);
+	});
+	it('should return its ID', () => {
+		const id = 1001;
+		expect(employee.getId()).toBe(id);
+	});
+	it('should return its email', () => {
+		const email = "test@gmail.com";
+		expect(employee.getEmail()).toBe(email);
+	});
+	it('should return Employee as the role', () => {
+		expect(employee.getRole()).toBe('Employee');
 	});
 });
