28784f91b51e52fdc35355200278698e29846234
jayarghargh <jayarghargh@gmail.com>
2021-02-01 13:00:16 +1030

    removed redundant override in classes for getRole method

---
+++ index.js
@@ -0,0 +1,12 @@
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const thisManager = new Manager("justin", 1001, "jayarghargh@gmail.com", 23);
+console.log(thisManager.getName(), thisManager.getRole());
+
+const thisEngineer = new Engineer("justin", 1001, "jayarghargh@gmail.com", "jayarghargh");
+console.log(thisEngineer.getRole());
+
+const thisIntern = new Intern("justin", 1001, "jayarghargh@gmail.com", "Monash");
+console.log(thisIntern.getRole());
